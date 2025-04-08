e3a2b849c1a3011278ab2cfb0c00fcfbff3cd083
aisyahhhhhh <203.aisyahhh@gmail.com>
2025-04-08 09:46:34 +0700

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
       'www.chassisvin.com', // Include this if you're also using images from this domain
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/window-sticker',
+        destination: '/window-sticker-by-vin',
+        permanent: true, // set to false if it's a temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
